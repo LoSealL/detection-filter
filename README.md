733d084c3c53b4dda581c13ef161d52df8e4b1be
Wenyi Tang <wenyi.tang@intel.com>
2023-08-29 09:24:48 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# DF - Detection Filter
+A mathematical toolbox to smooth the raw output from deep-learning detectors.
+
+### Spatial Filter
+
+### Temporal Filter
+
+## Installation
+
+```
+pip install df
+```
---
+++ df/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright Wenyi Tang 2023
+
+:Author: Wenyi Tang
+:Email: wenyitang@outlook.com
+
+A mathematical toolbox to smooth the raw output from deep-learning detectors.
+"""
+
+__version__ = "0.0.1"
